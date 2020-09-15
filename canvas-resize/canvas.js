8978c0a29e14e7c566d6753199b9d78f49c373a9
adreameryx <mt3178@srmist.edu.in>
2020-09-15 19:52:25 +0530

    canvas animations

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -34,7 +34,63 @@ console.log(canvas);
 // }
 
 
-c.beginPath();
-c.arc(300, 300, 30, 0, Math.PI * 2, false);
-c.strokeStyle = 'blue';
-c.stroke();
+function Circle (x, y , dx , dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+
+    this.draw = function() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = 'white';
+        c.stroke();
+    }
+    this.update = function() {
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+            
+        }
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+     
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    } 
+    
+}
+
+var circleArray = [];
+
+for ( var i = 0; i < 100; i++){
+    radius = 30;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.5) ;
+    var dy = (Math.random() - 0.5) ;
+    
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+    
+}
+console.log(circleArray);
+
+
+//circle.draw();
+
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0,0, innerWidth, innerHeight);
+    
+    for (var i = 0; i < circleArray.length; i++) {
+        circleArray[i].update()
+    }
+
+}
+animate();
+   
