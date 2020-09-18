8c5055ab7547e41ab167ad79df33dd37cd4edd59
adreameryx <mt3178@srmist.edu.in>
2020-09-18 23:36:24 +0530

    new commit

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -32,6 +32,18 @@ console.log(canvas);
 //     c.strokeStyle = 'blue';
 //     c.stroke();
 // }
+var mouse = {
+    x: undefined,
+    y: undefined
+}
+
+
+window.addEventListener('mousemove',
+function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    console.log(mouse);
+})
 
 
 function Circle (x, y , dx , dy, radius) {
@@ -44,7 +56,8 @@ function Circle (x, y , dx , dy, radius) {
     this.draw = function() {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = 'white';
+        c.strokeStyle = 'blue';
+        c.fill();
         c.stroke();
     }
     this.update = function() {
@@ -59,11 +72,20 @@ function Circle (x, y , dx , dy, radius) {
         this.x += this.dx;
         this.y += this.dy;
 
+        //  Interactivity
+        if (mouse.x - this.x < 50 && mouse.x -this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+            this.radius += 1;
+
+        } else {
+            this.radius -= 1;
+        }
+
+
         this.draw();
     } 
     
 }
-
+ 
 var circleArray = [];
 
 for ( var i = 0; i < 100; i++){
